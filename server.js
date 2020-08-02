08fa15e72a2a9d2e8457747f9e9b1eaabe702fa8
Aziz-pang <zhiming3399@gmail.com>
2020-08-02 21:46:05 +0800

    更新node-demo-1

--- server.js
+++ server.js
@@ -19,12 +19,20 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('你的房间有人进来' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+      <!DOCTYPE html>
+      <head>
+        <link rel="stylesheet" href='/x'/>
+      </head>
+      <body>
+        <h1>node的服务器开启</h1>
+      </body>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
